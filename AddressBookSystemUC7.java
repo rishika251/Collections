0fa4eade3b2e39804f0411827b8b6b1ec7253cc6
rishika251 <rishikasadineni@gmail.com>
2022-12-13 20:41:45 +0530

    Added:ability to find addressbook using collections

---
+++ AddressBookSystemUC7.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.collections;
+
+public class AddressBookSystemUC7 {
+	public static void main(String args[]) {
+		System.out.println("\n----------Welcome to Address Book--------");
+		Contact contact = new Contact();
+		contact.addressBook(contact);
+	
+	}
+
+}
